0bcd75b792cd1c0d7f162e8e7bd0c058c67c04aa
ppabis <piotr@pabis.eu>
2025-03-30 20:45:50 +0200

    Added VPC endpoints for ECR pulling

--- vpc.tf
+++ vpc.tf
@@ -18,3 +18,12 @@ module "vpc" {
     }
   }
 }
+
+module "vpc_endpoints" {
+  source                      = "git::https://github.com/aws-ia/terraform-aws-vpc_endpoints.git"
+  vpc_id                      = module.vpc.vpc_attributes.id
+  enabled_interface_endpoints = ["ecr_api", "ecr_dkr"]
+  enabled_gateway_endpoints   = ["s3"]
+  subnet_ids                  = values(module.vpc.private_subnet_attributes_by_az)[*].id
+  route_table_ids             = values(module.vpc.rt_attributes_by_type_by_az["private"])[*].id
+}
