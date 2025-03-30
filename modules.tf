37c6a6c5ffaf69ba9d0fea90a12d62a131e20e91
ppabis <piotr@pabis.eu>
2025-03-30 11:31:57 +0200

    Created empty ECS cluster using module

--- modules.tf
+++ modules.tf
@@ -1,3 +1,6 @@
+################################################################################
+# VPC
+################################################################################
 module "vpc" {
   source  = "aws-ia/vpc/aws"
   version = ">= 4.2.0"
@@ -14,4 +17,23 @@ module "vpc" {
       netmask = 24
     }
   }
+}
+
+################################################################################
+# ECS Cluster
+################################################################################
+module "ecs" {
+  source  = "terraform-aws-modules/ecs/aws"
+  version = "5.12.0"
+
+  cluster_name = "appconfig-demo"
+  cluster_settings = [
+    {
+      name  = "containerInsights"
+      value = "disabled"
+    }
+  ]
+
+  cloudwatch_log_group_retention_in_days = 14
+
 }
