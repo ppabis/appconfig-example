93c620ce080028a68740eaab71407ab89f07bb14
ppabis <piotr@pabis.eu>
2025-04-04 13:51:57 +0200

    Copy appconfig agent image to private ECR repo

--- ecr.tf
+++ ecr.tf
@@ -39,3 +39,25 @@ resource "docker_registry_image" "application_image" {
   name          = "${aws_ecr_repository.application_repository.repository_url}:${var.image_tag}"
   keep_remotely = true
 }
+
+########################################################
+# Repository for the AppConfig Agent to be pulled via VPC Endpoint
+########################################################
+resource "aws_ecr_repository" "appconfig_agent_repository" {
+  name = "appconfig-agent"
+}
+
+resource "docker_image" "appconfig_agent_image" {
+  name = "public.ecr.aws/aws-appconfig/aws-appconfig-agent:2.x"
+}
+
+resource "docker_tag" "appconfig_agent_image_tag" {
+  source_image = docker_image.appconfig_agent_image.image_id
+  target_image = "${aws_ecr_repository.appconfig_agent_repository.repository_url}:latest"
+}
+
+resource "docker_registry_image" "appconfig_agent_image" {
+  depends_on = [ docker_tag.appconfig_agent_image_tag ]
+  name          = "${aws_ecr_repository.appconfig_agent_repository.repository_url}:latest"
+  keep_remotely = true
+}
