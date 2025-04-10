0c7f10ebb1f6df6827e668f417e1a4caad0784b0
ppabis <piotr@pabis.eu>
2025-04-10 18:20:29 +0200

    Output s3 env file path

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,6 @@ output "alb_dns_name" {
   value = module.alb.dns_name
 }
 
+output "s3_env_file_path" {
+  value = "s3://${aws_s3_bucket.s3_bucket.bucket}/${aws_s3_bucket_object.environment_file.key}"
+}
