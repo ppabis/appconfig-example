321f68a08e95abd1783948ecfacc5c851a701fa4
ppabis <piotr@pabis.eu>
2025-04-11 08:27:43 +0200

    Add new config with feature flags

---
+++ appconfig_featureflags.tf
@@ -0,0 +1,45 @@
+resource "aws_appconfig_configuration_profile" "featureflags" {
+  application_id = aws_appconfig_application.appconfig_application.id
+  name           = "featureflags"
+  location_uri   = "hosted"
+  description    = "Feature flags example"
+  type = "AWS.AppConfig.FeatureFlags"
+}
+
+resource "aws_appconfig_hosted_configuration_version" "featureflags" {
+  application_id           = aws_appconfig_application.appconfig_application.id
+  configuration_profile_id = aws_appconfig_configuration_profile.featureflags.configuration_profile_id
+  content_type             = "application/json"
+  content                  = jsonencode({
+    flags: {
+      ff_rotate: {
+        name: "ff_rotate",
+        attributes: {
+          speed: {
+            constraints: {
+              type: "number",
+              required: true
+            }
+          }
+        }
+      }
+    },
+    values: {
+      ff_rotate: {
+        enabled: true,
+        speed: 10
+      }
+    },
+    version : "1"
+  })
+}
+
+resource "aws_appconfig_deployment" "featureflags" {
+  application_id           = aws_appconfig_application.appconfig_application.id
+  configuration_profile_id = aws_appconfig_configuration_profile.featureflags.configuration_profile_id
+  configuration_version    = aws_appconfig_hosted_configuration_version.featureflags.version_number
+  environment_id           = aws_appconfig_environment.live.environment_id
+  deployment_strategy_id   = "AppConfig.AllAtOnce"
+  description              = "Deployment of feature flags to live"
+}
+
