c25534af3ae86655aeae919e665d4c9b365f7d1e
ppabis <piotr@pabis.eu>
2025-04-17 07:45:28 +0200

    Add faster deployment strategy

--- appconfig.tf
+++ appconfig.tf
@@ -19,7 +19,7 @@ resource "aws_appconfig_hosted_configuration_version" "live_main" {
   configuration_profile_id = aws_appconfig_configuration_profile.prof.configuration_profile_id
   content_type             = "application/yaml"
   content                  = <<-EOF
-    background: '#1145cd'
+    background: '#cd0022'
     EOF
 }
 
@@ -28,7 +28,15 @@ resource "aws_appconfig_deployment" "live_main" {
   configuration_profile_id = aws_appconfig_configuration_profile.prof.configuration_profile_id
   configuration_version    = aws_appconfig_hosted_configuration_version.live_main.version_number
   environment_id           = aws_appconfig_environment.live.environment_id
-  deployment_strategy_id   = "AppConfig.AllAtOnce"
+  deployment_strategy_id   = aws_appconfig_deployment_strategy.AllAtOnce2Minutes.id
   description              = "Deployment of live environment"
 }
 
+resource "aws_appconfig_deployment_strategy" "AllAtOnce2Minutes" {
+  name = "AllAtOnce2MinutesFast"
+  description = "All at once deployment in 2 minutes bake"
+  final_bake_time_in_minutes = 2
+  deployment_duration_in_minutes = 0
+  growth_factor = 100
+  replicate_to = "NONE"
+}
--- appconfig_featureflags.tf
+++ appconfig_featureflags.tf
@@ -26,7 +26,7 @@ resource "aws_appconfig_hosted_configuration_version" "featureflags" {
     },
     values: {
       ff_rotate: {
-        enabled: true,
+        enabled: false,
         speed: 10
       }
     },
@@ -39,7 +39,7 @@ resource "aws_appconfig_deployment" "featureflags" {
   configuration_profile_id = aws_appconfig_configuration_profile.featureflags.configuration_profile_id
   configuration_version    = aws_appconfig_hosted_configuration_version.featureflags.version_number
   environment_id           = aws_appconfig_environment.live.environment_id
-  deployment_strategy_id   = "AppConfig.AllAtOnce"
+  deployment_strategy_id   = aws_appconfig_deployment_strategy.AllAtOnce2Minutes.id
   description              = "Deployment of feature flags to live"
 }
 
